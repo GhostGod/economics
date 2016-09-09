b174ebcee44c1aab48625dc12fe181aee6e64bc9
liuyang <y_liu@hiersun.com>
2016-09-09 13:45:33 +0800

    修改结构

---
+++ src/main/java/org/liuyang/Application.java
@@ -0,0 +1,17 @@
+package org.liuyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 启动入口
+ * @author liuyang
+ * @Email y_liu@hiersun.com | 745089707@qq.com
+ * @Date 2016年9月9日
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/org/liuyang/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author liuyang
+ * @Email y_liu@hiersun.com | 745089707@qq.com
+ * @Date 2016年9月9日
+ */
+package org.liuyang;
